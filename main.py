d4796b3692ee0f6e2d0ce35f87da6cc40992bf29
shreya-uttam309 <shreya.uttam.09@gmail.com>
2025-01-06 01:21:45 +0530

    Final Commit

--- main.py
+++ main.py
@@ -31,11 +31,45 @@ resources={
 }
 
 def is_resource_sufficient(order_ingredients):
+    """_summary_
+ returns true if order can be made and false if ingredients are insufficient
+    """
     for item in order_ingredients:
         if order_ingredients[item]>resources[item]:
-            print("Sorry there is not enough {item}.")
+            print(f"Sorry there is not enough {item}.")
             return False
     return True
+
+
+def process_coins():
+    """Returns total calculated from coins inserted
+    """
+    print("Please insert coins")
+    total=int(input("How many quarters? :"))*0.25
+    total+=int(input("How many dimes? :"))*0.1
+    total+=int(input("How many nickles? :"))*0.05
+    total+=int(input("How many pennies? :"))*0.01
+    return total
+    
+def is_transaction_successfull(money_received, drink_cost):
+    """retturns true if payment is accepted and false  if money is insufficient"""
+    if money_received>=drink_cost:
+        change=round(money_received-drink_cost,2)
+        print(f"Here is ${change} in change")
+        global profit
+        profit+=drink_cost
+        return True
+    else:
+        print("Sorry that's not enough money. Money refunded.")
+        return False
+    
+    
+def make_coffee(drink_name, order_ingredients):
+    """ deduct the required ingredients from the resources"""
+    for item in order_ingredients:
+        resources[item]-=order_ingredients[item]
+    print(f"Here is your ${drink_name}☕")
+    
 is_on=True
 
 while is_on:
@@ -49,6 +83,7 @@ while is_on:
         print(f"Money: ${profit}")
     else:
         drink=MENU[choice]
-        is_resource_sufficient(drink["ingredients"])
-
-    
+        if is_resource_sufficient(drink["ingredients"]):
+            payment=process_coins()
+            if(is_transaction_successfull(payment,drink["cost"])):
+                make_coffee(choice,drink["ingredients"])
